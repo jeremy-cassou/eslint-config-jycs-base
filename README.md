41a68a92cabf10a3d2ffbfe9307e06e3434cd68c
Jeremy Cassou <jeremy.cassou@gmail.com>
2020-12-04 13:55:54 +0100

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# @jycs/eslint-config-jycs-base
+
+ESLint rule set for JavaScript projects
+
+## Install
+```
+npm install --save-dev eslint eslint-config-airbnb-base eslint-import-resolver-alias
+npm install --save-dev @jycs/eslint-config-jycs-base
+```
+
+# .eslintrc.js file example
+```javascript
+module.exports = {
+  root: true,
+  extends: ['@jycs/eslint-config-jycs-vue'],
+}
+```
+
+# Husky config example
+Example of usage with Husky for check lint before commit
+(Also includes lint for commit message with conventional commit)
+```json
+{ // package.json
+  "scripts": {
+    "lint": "eslint ."
+  },
+  "devDependencies": {
+    "@commitlint/cli": "^11.0.0",
+    "@commitlint/config-conventional": "^11.0.0",
+    "eslint": "^6.7.2",
+    "eslint-config-airbnb-base": "^14.2.0",
+    "eslint-import-resolver-alias": "^1.1.2",
+    "eslint-plugin-import": "^2.22.1",
+    "husky": "^4.3.0",
+    "lint-staged": "^10.4.0",
+  },
+  "lint-staged": {
+    "*.{js,vue}": [
+      "npm run lint",
+      "git add"
+    ]
+  },
+  "husky": {
+    "hooks": {
+      "commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
+      "pre-commit": "lint-staged"
+    }
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+
+  env: {
+    browser: true,
+  },
+
+  settings: {
+    // Aliases
+    'import/resolver': {
+      alias: {
+        map: [
+          ['src', './src'],
+        ],
+        extensions: ['.js', '.ts', '.vue'],
+      },
+    },
+  },
+
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+  ],
+
+  rules: {
+    // Standard Rules
+    semi: ['error', 'never'],
+    'space-before-function-paren': ['error', 'always'],
+    'arrow-parens': ['error', 'as-needed'],
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'no-console': process.env.NODE_ENV === 'production'
+      ? ['error', { allow: ['warn', 'error'] }]
+      : 'off',
+    'no-param-reassign': ['error', { props: false }],
+
+    // Import Rules
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never',
+    }],
+  },
+
+  // Unit Test
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
